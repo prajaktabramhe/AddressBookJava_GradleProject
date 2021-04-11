f6259e9f8eca522e094a15aeb06d4d74f33f0b01
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-04-12 00:18:30 +0530

    [Prajakta] : Createdd method jsonWriteData and jsonReadData for read an write data and import gson library

--- src/main/java/com/adddressbook/JsonFile.java
+++ src/main/java/com/adddressbook/JsonFile.java
@@ -1,2 +1,83 @@
-package com.adddressbook;public class JsonFile {
+package com.adddressbook;
+
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class JsonFile
+{
+    private static final String HOME = System.getProperty("user.dir");
+    private static final String fileName = "AddressBook.json";
+    private static final Path homePath = Paths.get(HOME);
+
+    private static final Gson gson = new GsonBuilder().create();
+
+    public boolean jsonWriteData(Contact contactinfo)
+    {
+        if (Files.exists(homePath))
+        {
+            Path filePath = Paths.get(HOME + "/" + fileName);
+            try {
+
+                if (Files.exists(filePath))
+                {
+                    String s = gson.toJson(contactinfo);
+                    FileWriter fileWriter = new FileWriter(String.valueOf(filePath));
+                    fileWriter.write(s);
+                    fileWriter.close();
+                    return true;
+                } else {
+                    Files.createFile(filePath);
+                    String s = gson.toJson(contactinfo);
+                    FileWriter fileWriter = new FileWriter(String.valueOf(filePath));
+                    fileWriter.write(s);
+                    fileWriter.close();
+                    return true;
+                }
+            } catch (IOException e)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean jsonReadData()
+    {
+        if (Files.exists(homePath))
+        {
+            Path filePath = Paths.get(HOME + "/" + fileName);
+            try {
+                if (Files.exists(filePath))
+                {
+                    BufferedReader br = new BufferedReader(new FileReader(String.valueOf(filePath)));
+                    Contact contactReader = gson.fromJson(br, Contact.class);
+                    System.out.println("contactReader{" +
+                            "firstName='" + contactReader.firstName + '\'' +
+                            ", lastName='" + contactReader.lastName + '\'' +
+                            ", address='" + contactReader.address + '\'' +
+                            ", city='" + contactReader.city + '\'' +
+                            ", state='" + contactReader.state + '\'' +
+                            ", zip=" + contactReader.zip +
+                            ", MobileNo.='" + contactReader.mobileNo + '\'' +
+                            ", email='" + contactReader.email + '\'' +
+                            '}' );
+                    return true;
+                }
+            } catch (IOException e)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 }
